d6ada9a2eefb748078a2dcf60bc91a982506064a
dimireme <a_s_d@mail.ru>
2018-07-24 15:40:08 +0300

    Blank application for test config files.

---
+++ src/client/app/App.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+export default class App extends PureComponent {
+	render() {
+		return (
+			<div>
+				<h2>Hello from react app</h2>
+			</div>
+		);
+	}
+}
