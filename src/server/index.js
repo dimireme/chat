6d9150e9c704f02543831eb9c5b1c017d1bd63d7
dimireme <a_s_d@mail.ru>
2018-07-30 22:34:08 +0300

    Code refactoring.

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
-const cors = require('cors');
-const socket = require('socket.io');
-const path = require('path');
-const fs = require('fs');
-
 const app = express();
-const dataFile = path.join(__dirname, 'data.json');
+const socket = require('socket.io');
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+
+const DATAFILE = path.join(__dirname, 'data.json');
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+
 app.get('/getHistory', (req, res) => {
-	fs.readFile(dataFile, 'utf8', (err, data) => {
-		if (err) console.error(err);
+	fs.readFile(DATAFILE, 'utf8', (err, data) => {
+		if (err) {
+			console.error(err);
+			res.send("Error " + err);
+		}
 
 		// get only last 3 messages.
 		const result = JSON.parse(data).slice(-3);
@@ -19,8 +24,8 @@ app.get('/getHistory', (req, res) => {
 	});
 });
 
-const server = app.listen(3000, () => {
-	console.log('Running at port 3000.');
+const server = app.listen(PORT, () => {
+	console.log(`Listening on ${PORT}.`);
 });
 
 const io = socket(server);
@@ -29,17 +34,7 @@ io.on('connection', (socket) => {
 	console.log(`Client with id ${socket.id} was connected.`);
 
 	socket.on('message', (message) => {
-		// save message to the data file
-		fs.readFile(dataFile, 'utf8', (err, data) => {
-			if (err) console.error(err);
-
-			const result = JSON.parse(data);
-			result.push(message);
-			fs.writeFile(dataFile, JSON.stringify(result), (err) => {
-				if (err) console.error(err);
-			});
-		});
-
+		saveMessage(message);
 		io.emit('message', message);
 	});
 
@@ -47,3 +42,15 @@ io.on('connection', (socket) => {
 		console.log(`Client with id ${socket.id} was disconnected.`);
 	});
 });
+
+const saveMessage = function(message) {
+	// save message to the data file
+	fs.readFile(DATAFILE, 'utf8', (err, data) => {
+		if (err) console.error(err);
+		const result = JSON.parse(data);
+		result.push(message);
+		fs.writeFile(DATAFILE, JSON.stringify(result), (err) => {
+			if (err) console.error(err);
+		});
+	});
+};
